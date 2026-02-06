135b7c9a129cbb763377fc8cfe8f4179feeb0292
Vizsga01 VisualStudio <vsvizsga01@dozsakalocsa.hu>
2026-02-06 12:11:41 +0100

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sziasztok! B. Gábor vagyok!");
+
+            Console.ReadKey();
+        }
+    }
+}
